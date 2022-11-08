5dd4014619cff3530401b9ddeaf83ee4aee6b59f
sushilpandey-coditas <sushil.pandey@coditas.com>
2022-11-08 15:13:45 +0530

    updated SG inbound rules

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -79,6 +79,18 @@ resource "aws_security_group" "sg" {
         cidr_blocks     = ["0.0.0.0/0"]
     }
     ingress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    ingress {
         from_port       = 0
         to_port         = 0
         protocol        = "icmp"
